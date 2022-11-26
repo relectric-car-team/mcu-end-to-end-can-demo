542e08ba473898f00072a1db2458d69b000bebc3
Ahmad <100788678+AhmadJanjua@users.noreply.github.com>
2022-11-26 12:52:51 -0700

    Can bus code
    Boiler plate can bus code.

--- main.c
+++ main.c
@@ -6,28 +6,60 @@
  */
 
 #include "mcc_generated_files/mcc.h" 
+#include "mcc_generated_files/can1.h"
+#include "mcc_generated_files/can_types.h"
+#define CAN_TX_BUFF  "TEMPERATURE SENSOR ON"
 
 /*
                          Main application
  */
-void main(void)
+//void main(void)
+//{
+//    // Initialize the device
+//    SYSTEM_Initialize();
+//
+//    // Enable the Global Interrupts
+//    //INTERRUPT_GlobalInterruptEnable();
+//
+//    // Disable the Global Interrupts
+//    //INTERRUPT_GlobalInterruptDisable();
+//    int8_t counter = 0;
+//    while (1)
+//    {
+//        //__delay_ms(100);
+//        // Writing -> serial bus 
+//        printf("%d\n", counter);
+//        counter++;
+//        // Reading <- serial bus
+//        
+//    }
+//}
+
+void main(void) 
 {
-    // Initialize the device
+    CAN_MSG_OBJ msg;
+    uint8_t data[32] = CAN_TX_BUFF;
+
     SYSTEM_Initialize();
 
-    // Enable the Global Interrupts
-    //INTERRUPT_GlobalInterruptEnable();
-
-    // Disable the Global Interrupts
-    //INTERRUPT_GlobalInterruptDisable();
-    int8_t counter = 0;
-    while (1)
+    if(CAN_CONFIGURATION_MODE == CAN1_OperationModeGet())
     {
-        //__delay_ms(100);
-        // Writing -> serial bus 
-        printf("%d\n", counter);
-        counter++;
-        // Reading <- serial bus
-        
+        if(CAN_OP_MODE_REQUEST_SUCCESS == CAN1_OperationModeSet(CAN_NORMAL_2_0_MODE))
+        {
+            msg.msgId = 0x1FFFF;
+            msg.field.formatType = CAN_FD_FORMAT;
+            msg.field.brs = CAN_NON_BRS_MODE;
+            msg.field.frameType = CAN_FRAME_DATA;
+            msg.field.idType = CAN_FRAME_EXT;
+            msg.field.dlc = DLC_32;
+            msg.data = data;
+
+            if(CAN_TX_FIFO_AVAILABLE == (CAN1_TransmitFIFOStatusGet(CAN1_TX_TXQ) & CAN_TX_FIFO_AVAILABLE))
+            {
+                CAN1_Transmit(CAN1_TX_TXQ, &msg);
+            }
+        }
     }
+
+    while(1);
 }
